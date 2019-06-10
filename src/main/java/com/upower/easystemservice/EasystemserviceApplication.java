3bfd32fbe557b0e2dd7c8f93d9dec6ff38938fa9
molingVae <1542664947@qq.com>
2019-06-10 18:50:37 +0800

    first commit

---
+++ src/main/java/com/upower/easystemservice/EasystemserviceApplication.java
@@ -0,0 +1,13 @@
+package com.upower.easystemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasystemserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasystemserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upower/easystemservice/EasystemserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.upower.easystemservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasystemserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
